b4c45bfc5af8208a96dfdba2fd228205e73534e7
Zistus Ndu <sunday@ajocard.com>
2020-03-29 04:49:45 +0100

    Fixed lint errors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require('express');
+
 const requestValidator = require('express-validator');
+
 const cors = require('cors');
+
 const morgan = require('morgan');
+
 const dotenv = require('dotenv');
 
 const result = dotenv.config();
